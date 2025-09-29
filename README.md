019b2ad36848a40f82da865010efaa961e828fc4
Visha118 <v.recess@gmail.com>
2025-09-29 23:57:11 +0530

    s3 bucket, dynamodb table resource creation

---
+++ README.md
@@ -0,0 +1,2 @@
+🚀 Create a S3 Bucket (AWS)
+Goal: To create a s3 bucket for remote state file management.
---
+++ main.tf
@@ -0,0 +1,16 @@
+resource "aws_s3_bucket" "this" {
+  bucket = "tf-s3-state-bucket-1"
+  force_destroy = true
+}
+
+#dynamo db table for state locking
+resource "aws_dynamodb_table" "tf_state_lock_table" {
+  name         = "terraform-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
